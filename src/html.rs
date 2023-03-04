23a07f890f56f55ad45524a9acdad531b98e653d
Adam Lipson <40801946+Adamlip1334@users.noreply.github.com>
2023-03-04 16:47:42 -0500

    finished parsing html

--- src/html.rs
+++ src/html.rs
@@ -1,7 +1,30 @@
+use crate::dom;
+use std::collections::HashMap;
+
+pub fn parse(source: String) -> dom::Node {
+    let mut nodes = Parser {
+        pos: 0,
+        input: source,
+    }
+    .parse_nodes();
+
+    // creates root element if it doesn't exist
+    if nodes.len() == 1 {
+        nodes.swap_remove(0)
+    } else {
+        dom::elem("html".to_string(), HashMap::new(), nodes)
+    }
+}
+
+struct Parser {
+    pos: usize,
+    input: String,
+}
+
 impl Parser {
-    /// Parse a sequence of sibling nodes.
+    // parse a sequence of sibling nodes.
     fn parse_nodes(&mut self) -> Vec<dom::Node> {
-        let mut nodes = vec!();
+        let mut nodes = vec![];
         loop {
             self.consume_whitespace();
             if self.eof() || self.starts_with("</") {
@@ -12,13 +35,111 @@ impl Parser {
         nodes
     }
 
-    /// Parse a single node.
+    // parses  a single node.
     fn parse_node(&mut self) -> dom::Node {
         match self.next_char() {
             '<' => self.parse_element(),
-            _   => self.parse_text()
+            _ => self.parse_text(),
         }
     }
 
-}
+    /// parse a single element (opening tag, content, closing tag)
+    fn parse_element(&mut self) -> dom::Node {
+        //open tag
+        assert_eq!(self.consume_char(), '<');
+        let tag_name = self.parse_tag_name();
+        let attrs = self.parse_attributes();
+        assert_eq!(self.consume_char(), '>');
 
+        // content
+        let children = self.parse_nodes();
+
+        // closing tag
+        assert_eq!(self.consume_char(), '<');
+        assert_eq!(self.consume_char(), '/');
+        assert_eq!(self.parse_tag_name(), tag_name);
+        assert_eq!(self.consume_char(), '>');
+
+        dom::elem(tag_name, attrs, children)
+    }
+
+    fn parse_tag_name(&mut self) -> String {
+        self.consume_while(|c| match c {
+            'a'..='z' | 'A'..='Z' | '0'..='9' => true,
+             _ => false,
+        })
+    }
+
+
+    // parse the name value pairs by, seperate by white space
+    fn parse_attributes(&mut self) -> dom::AttrMap {
+        let mut attributes = HashMap::new();
+        loop {
+            self.consume_whitespace();
+            if self.next_char() == '>' {
+                break;
+            }
+            let (name, value) = self.parse_attr();
+            attributes.insert(name, value);
+        }
+        attributes
+    }
+
+
+    fn parse_attr(&mut self) -> (String, String) {
+        let name = self.parse_tag_name();
+        assert_eq!(self.consume_char(), '=');
+        let value = self.parse_attr_value();
+        (name, value)
+    }
+
+    // parse a quoted value
+    fn parse_attr_value(&mut self) -> String {
+        let open_quote = self.consume_char();
+        assert!(open_quote == '"' || open_quote == '\'');
+        let value = self.consume_while(|c| c != open_quote);
+        assert_eq!(self.consume_char(), open_quote);
+        value
+    }
+
+    // parse a text node
+    fn parse_text(&mut self) -> dom::Node {
+        dom::text(self.consume_while(|c| c != '<'))
+    }
+
+    fn consume_whitespace(&mut self) {
+        self.consume_while(char::is_whitespace);
+    }
+
+    fn consume_while<F>(&mut self, test: F) -> String
+            where F: Fn(char) -> bool {
+        let mut result = String::new();
+        while !self.eof() && test(self.next_char()) {
+            result.push(self.consume_char());
+        }
+        result
+    }
+
+    // return the current character, and advance self.pos to the next character
+    fn consume_char(&mut self) -> char {
+        let mut iter = self.input[self.pos..].char_indices();
+        let (_, cur_char) = iter.next().unwrap();
+        let (next_pos, _) = iter.next().unwrap_or((1, ' '));
+        self.pos += next_pos;
+        cur_char
+    }
+
+    // read the current character without consuming it
+    fn next_char(&self) -> char {
+        self.input[self.pos..].chars().next().unwrap()
+    }
+
+    fn starts_with(&self, s: &str) -> bool {
+        self.input[self.pos ..].starts_with(s)
+    }
+
+    /// return true if all input is consumed
+    fn eof(&self) -> bool {
+        self.pos >= self.input.len()
+    }
+}
